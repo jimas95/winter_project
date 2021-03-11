f737ff0a7dbbc559a5c907f9792749ce0da841b2
jimas95 <jimas95@hotmail.com>
2021-03-10 21:26:42 -0600

    grabing...

--- commander/nodes/manipulation.py
+++ commander/nodes/manipulation.py
@@ -5,6 +5,8 @@ import rospy
 import sys
 from geometry_msgs.msg import Pose, Quaternion
 import time
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+import tf
 
 #saved states 
 snake = [-1.5797138671875, -1.63575, -1.6687451171875, -0.175255859375, -0.635630859375, -1.8552236328125, -4.6607734375]
@@ -22,6 +24,10 @@ class Manipulator:
         # self.right_gripper = Gripper('hand')
         # self.right_gripper.calibrate(timeout=1.0)
 
+
+        #tf listener
+        self.listener = tf.TransformListener()
+
     def go_to_state(self,joint_goal):
         self.group.set_joint_value_target(joint_goal)
         (result, plan, frac, errCode) = self.group.plan()
@@ -37,11 +43,57 @@ class Manipulator:
         # self.group.clear_pose_target(end_effector_link = "hand")
 
 
+    def get_offset(self):
+        print(self.group.get_current_pose().pose)
+        try:
+            (trans,rot) = self.listener.lookupTransform('/base_link', '/tag_0', rospy.Time(0))
+            print(trans)
+            print("diff")
+            pose = self.group.get_current_pose()
+            eef_pose = [pose.pose.position.x-trans[0],pose.pose.position.y-trans[1],pose.pose.position.z-trans[2]]
+            print(eef_pose)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            print("no tf found")
+
+
+
+
     def get_position(self):
         pos = self.group.get_current_pose("right_hand")
         rospy.loginfo(f"ARM Position --> {pos}")
 
 
+    def check_tf(self):
+        # print(sel f.group.get_current_pose())
+        try:
+            (trans,rot) = self.listener.lookupTransform('/base_link', '/tag_0', rospy.Time(0))
+            print (trans)
+            return True
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            return False
+        return False
+
+
+    def grab_object(self, off):
+        (trans,rot) = self.listener.lookupTransform('/base_link', '/tag_0', rospy.Time(0))
+        
+        print(trans)
+        trans[0] = trans[0] + off[0]
+        trans[1] = trans[1] + off[1]
+        trans[2] = trans[2] + off[2]
+        quad = [1,0,0,0]
+        pose = trans+quad
+        self.go_to_pose(pose)
+
+    def pick(self):
+        
+        is_tf = self.check_tf()
+        if(is_tf):
+            self.go_to_state(snake2)
+            self.grab_object([0.1,0.0,0.1])
+            self.grab_object([0.1,0.0,-0.02])
+            # close gripper
+            self.go_to_state(snake2)
 
 def main():
     """ The main() function. """
@@ -49,11 +101,29 @@ def main():
     rospy.init_node('arm',log_level=rospy.DEBUG)
     handler = Manipulator()
 
-
+    handler.go_to_state(snake2)
     while(not rospy.is_shutdown()):
         #handler.get_position()
-        handler.go_to_state(snake2)
-        handler.go_to_state(snake)
+        # handler.go_to_state(snake2)
+        # handler.go_to_state(snake)
+        is_tf = handler.check_tf()
+        print(is_tf)
+        # handler.get_offset()
+        if(is_tf):
+            handler.grab_object([0.1,0.0,0.1])
+            handler.grab_object([0.1,0.0,-0.02])
+          #  handler.grab_object([0,0,0.2])
+            #handler.grab_object([0,0,0.1])
+
+            #handler.grab_object([0.1,0,0.3])
+            #handler.grab_object([0.2,0,0.3])
+            exit()
+
+
+        
+            
+
+
         rospy.sleep(1)
     
 
