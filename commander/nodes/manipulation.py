c826936923b93f91ef7df49c488093b1af1d8fd1
jimas95 <jimas95@hotmail.com>
2021-03-10 21:56:32 -0600

    open close gripper

--- commander/nodes/manipulation.py
+++ commander/nodes/manipulation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from moveit_commander import roscpp_initialize
 import moveit_commander
+import actionlib 
 import rospy
 import sys
 from geometry_msgs.msg import Pose, Quaternion
@@ -11,6 +12,9 @@ import tf
 #saved states 
 snake = [-1.5797138671875, -1.63575, -1.6687451171875, -0.175255859375, -0.635630859375, -1.8552236328125, -4.6607734375]
 snake2 = [0.1599619140625, -0.698787109375, -0.2838037109375, 0.994494140625, 0.2812392578125, 0.87126953125, -1.6042626953125]
+
+from control_msgs.msg import GripperCommandAction, GripperCommandGoal
+
 class Manipulator:
     """ Helper class for node arm_control.
     Will sort cups then build tower by grabbing and placing cups using either one hand or both hands.
@@ -21,12 +25,29 @@ class Manipulator:
         self.robot = moveit_commander.RobotCommander(robot_description='robot_description', ns="robot")
         self.scene = moveit_commander.PlanningSceneInterface()
         self.group = moveit_commander.MoveGroupCommander("right_arm", robot_description='robot_description')
+
+        self.client = actionlib.SimpleActionClient('/robot/end_effector/right/gripper_action/', GripperCommandAction)
+
         # self.right_gripper = Gripper('hand')
         # self.right_gripper.calibrate(timeout=1.0)
 
 
         #tf listener
         self.listener = tf.TransformListener()
+    
+    def open_gripper(self):
+        goal = GripperCommandGoal()
+        goal.command.position= 0.2
+        # Fill in the goal here
+        self.client.send_goal(goal)
+        self.client.wait_for_result(rospy.Duration.from_sec(5.0))
+
+    def close_gripper(self):
+        goal = GripperCommandGoal()
+        goal.command.position= 0
+        # Fill in the goal here
+        self.client.send_goal(goal)
+        self.client.wait_for_result(rospy.Duration.from_sec(5.0))
 
     def go_to_state(self,joint_goal):
         self.group.set_joint_value_target(joint_goal)
@@ -108,15 +129,12 @@ def main():
         # handler.go_to_state(snake)
         is_tf = handler.check_tf()
         print(is_tf)
+        handler.toggle_gripper()
         # handler.get_offset()
         if(is_tf):
-            handler.grab_object([0.1,0.0,0.1])
-            handler.grab_object([0.1,0.0,-0.02])
-          #  handler.grab_object([0,0,0.2])
-            #handler.grab_object([0,0,0.1])
-
-            #handler.grab_object([0.1,0,0.3])
-            #handler.grab_object([0.2,0,0.3])
+            #handler.grab_object([0.1,0.0,0.1])
+            #handler.grab_object([0.1,0.0,-0.02])
+           # handler.toggle_gripper()
             exit()
 
 
